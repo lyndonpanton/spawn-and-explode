0f82575e8287eab8c2363b508b8fc83664e5f5ff
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-11 05:57:43 +0000

    Completed problem 2
    A teddy bear is now spawned once at the mouse location every time the left mouse key is pressed

--- Exercise6/Assets/Scripts/MouseButtonProcessor.cs
+++ Exercise6/Assets/Scripts/MouseButtonProcessor.cs
@@ -16,12 +16,40 @@ public class MouseButtonProcessor : MonoBehaviour
     bool spawnInputOnPreviousFrame = false;
 	bool explodeInputOnPreviousFrame = false;
 
+    // only trigger action on initial mouse button down trigger
+    bool isLeftMouseButtonDown = false;
+
 	/// <summary>
 	/// Update is called once per frame
 	/// </summary>
 	void Update()
 	{
         // spawn teddy bear as appropriate
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isLeftMouseButtonDown)
+            {
+                isLeftMouseButtonDown= true;
+
+                GameObject teddyBear;
+
+                Vector3 mouseScreenPosition = Input.mousePosition;
+                mouseScreenPosition.z = -Camera.main.transform.position.z;
+
+                Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(
+                    mouseScreenPosition
+                );
+
+                teddyBear = Instantiate<GameObject>(
+                    prefabTeddyBear,
+                    mouseWorldPosition,
+                    Quaternion.identity
+                );
+            }
+        } else
+        {
+            isLeftMouseButtonDown = false;
+        }
 
         // explode teddy bear as appropriate
 		
